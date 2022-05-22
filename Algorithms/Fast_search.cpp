8b6469eec65ca82bdefde68b9e71e8e5c9cb77e1
Mohamed Moumni <mohamedmoumni593@gmail.com>
2022-05-22 21:38:33 +0100

    solving B and C problems in Binary search

---
+++ Algorithms/Fast_search.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+void    solve(vector<long>& vect, long x, long y, long n)
+{
+    
+}
+
+
+int main (void)
+{
+    long n,k;
+
+    cin >> n;
+    vector<long> tab(n);
+    for (int i=0; i < n ; i++)
+    {
+        cin >> tab[i];
+    }
+    cin >> k;
+    while (k--)
+    {
+        long x,y;
+        cin >> x >> y;
+    }
+}
