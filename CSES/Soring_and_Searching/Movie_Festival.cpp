e92fb5b349997d3098f41aef7ea05c7aaabd741a
Mohamed Moumni <mmoumni@student.1337.ma>
2022-08-09 18:45:13 +0100

    Solving Movie_Festival porblem

--- CSES/Soring_and_Searching/Movie_Festival.cpp
+++ CSES/Soring_and_Searching/Movie_Festival.cpp
@@ -1,1 +1,43 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+bool sortbysecond(const pair<long, long> &a, const pair<long, long> &b)
+{
+    return (a.second < b.second);
+}
+
+int main(void)
+{
+    long n;
+    vector<pair<long, long> > tab;
+
+    cin >> n;
+    long a;
+    long b;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        cin >> b;
+        tab.push_back(make_pair(a, b));
+    }
+    sort(tab.begin(), tab.end(), sortbysecond);
+    long res = 0;
+    long endOfLastMovie = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (tab[i].first >= endOfLastMovie)
+        {
+            res++;
+            endOfLastMovie = tab[i].second;
+        }
+    }
+    cout << res << "\n";
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << tab[i].first << " " << tab[i].second << "\n";
+    // }
+    return (0);
+}
