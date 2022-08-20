f317a12e5f6a9c27b02189b5730d65b2e8698ff6
Mohamed-Moumni <mmoumni@student.1337.ma>
2022-08-20 22:21:33 +0100

    collecting numbers

--- CSES/Soring_and_Searching/Collecting_numbers.cpp
+++ CSES/Soring_and_Searching/Collecting_numbers.cpp
@@ -11,16 +11,18 @@ int main(void)
     cin >> n;
     long long ans = 0;
     vector<long long> tab(n);
+    vector<long long> pos(n);
     for (int i = 0; i < n; i++)
     {
         cin >> tab[i];
+        pos[tab[i]] = i;
     }
     int i = 0;
     while (i < n)
     {
-        if ((i + 1) < n && tab[i] > tab[i + 1])
+        if (pos[i] > pos[i + 1])
             ans++;
         i++;
     }
-    cout << (ans) << "\n";
+    cout << (ans + 1) << "\n";
 }
