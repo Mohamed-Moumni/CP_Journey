7e73705c7eb4cd72c6baba7541ae5617b5cfd6bb
Mohamed-Moumni <mohamedmoumni593@gmail.com>
2022-09-02 08:50:50 +0100

    solving Coder.cpp problem

---
+++ Algorithms/Implementation/Coder.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <complex>
+#include <iomanip>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <set>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+#define int long long
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<pair <int,int> > vpi;
+typedef vector<long long> vll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+#define F first
+#define ln '\n'
+#define S second
+#define PB push_back
+#define MP make_pair
+#define B begin()
+#define RB rbegin()
+#define E end()
+#define RE rend()
+#define Z size()
+#define REP(i,a,b) for (int i = a; i < b; i++)
+#define L length()
+#define valid(a,b,c) (a >= b && a < c ? 1 : 0)
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
+const int mod = (int)1e9 + 7;
+
+signed main(void)
+{
+    int n;
+
+    cin >> n;
+    if (n == 1)
+    {
+        cout << 1 << endl;
+        cout << "C" << endl;
+        return (0);
+    }
+    if (n % 2)
+    {
+        cout << (((n/2) * (n/2)) + ((n - n/2) * (n - n/2))) << endl;
+    }
+    else
+    {
+        cout << (n * n / 2) << endl;
+    }
+        for (int i =0 ;i < n; i++)
+        {
+            for (int j = 0 ; j < n; j++)
+            {
+                if ((i % 2 == 0 && j % 2 == 0) || (i % 2 && j % 2))
+                {
+                    cout << "C";
+                }
+                else
+                {
+                    cout << ".";
+                }
+            }
+            cout << endl;
+        }
+}
