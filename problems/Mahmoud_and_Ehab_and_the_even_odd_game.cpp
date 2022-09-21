f9aebdb3c37db537660e6a879c679ca2675a1d69
Mohamed-Moumni <mohamedmoumni593@gmail.com>
2022-09-21 12:47:40 +0100

    solving some problems Codefroces Div.2A

---
+++ problems/Mahmoud_and_Ehab_and_the_even_odd_game.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <complex>
+#include <iomanip>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <set>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+#define int long long
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<pair <int,int> > vpi;
+typedef vector<long long> vll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+#define F first
+#define ln '\n'
+#define S second
+#define PB push_back
+#define MP make_pair
+#define B begin()
+#define RB rbegin()
+#define E end()
+#define RE rend()
+#define Z size()
+#define REP(i,a,b) for (int i = a; i < b; i++)
+#define L length()
+#define valid(a,b,c) (a >= b && a < c ? 1 : 0)
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
+const int mod = (int)1e9 + 7;
+
+void solve(void)
+{
+    int n;cin>>n;
+    if (n%2 == 0)
+        cout << "Mahmoud" << ln;
+    else
+        cout << "Ehab" << ln;
+}
+
+signed main(void)
+{
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
