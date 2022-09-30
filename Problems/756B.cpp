868aaca5f6406a4eabfb4bb483922fcb3c4cd08d
Mohamed-Moumni <mohamedmoumni593@gmail.com>
2022-09-30 11:12:12 +0100

    solving two problems in code forces 756B and 867A

---
+++ Problems/756B.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <complex>
+#include <iomanip>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <set>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+#define int long long
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<pair <int,int> > vpi;
+typedef vector<long long> vll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+#define F first
+#define ln '\n'
+#define S second
+#define PB push_back
+#define MP make_pair
+#define B begin()
+#define RB rbegin()
+#define E end()
+#define RE rend()
+#define Z size()
+#define REP(i,a,b) for (int i = a; i < b; i++)
+#define L length()
+#define valid(a,b,c) (a >= b && a < c ? 1 : 0)
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
+const int mod = (int)1e9 + 7;
+
+void solve(void)
+{
+    int n;cin>>n;
+    string s;cin>>s;
+    string ans = "";
+    for (int i=0;i < s.size() ;i++)
+    {
+        if (n % 2)
+        {
+            ans = ans + string(1,s[i]);
+        }
+        else
+        {
+            ans = string(1, s[i]) + ans;
+        }
+        n--;
+    }
+    cout << ans << ln;
+}
+
+signed main(void)
+{
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
---
+++ Problems/ProblmeSets.md
@@ -0,0 +1,4 @@
+# Problems Sets Links
+
+* [756B](https://codeforces.com/problemset/problem/746/B)
+* [867A](https://codeforces.com/problemset/problem/867/A)
