97a4a0d7c64fd063156980a2250ce43c8cba3622
Mohamed-Moumni <mohamedmoumni593@gmail.com>
2022-10-11 17:49:41 +0100

    solving A and B in Codeforces 826 div3

---
+++ Contests/Codeforces_Round_826_Div3/B.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <complex>
+#include <iomanip>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <set>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+#define int long long
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<pair <int,int> > vpi;
+typedef vector<long long> vll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+#define F first
+#define ln '\n'
+#define S second
+#define PB push_back
+#define MP make_pair
+#define B begin()
+#define RB rbegin()
+#define E end()
+#define RE rend()
+#define Z size()
+#define REP(i,a,b) for (int i = a; i < b; i++)
+#define L length()
+#define valid(a,b,c) (a >= b && a < c ? 1 : 0)
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
+const int mod = (int)1e9 + 7;
+
+void solve(void)
+{
+    int n;cin>>n;
+    vi a;
+    if (n == 3)
+        cout << "-1";
+    else if (n == 2)
+        cout << "2 1";
+    else if (n == 4)
+        cout << "4 3 2 1";
+    else
+    {
+        cout << n << " ";
+        if (n - 4 > 0)
+            cout << n - 1 << " ";
+        cout << "1 2 3 ";
+        int k = n - 5;
+        int i = 4;
+        while (k > 0)
+        {
+            cout << i << " ";
+            i++;
+            k--;
+        }
+    }
+    cout << ln;
+}
+
+signed main(void)
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
