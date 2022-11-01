e8b7bb67ffff4b7914778a66eda7d10c806c1e02
Mohamed-Moumni <mohamedmoumni593@gmail.com>
2022-11-01 14:48:23 +0100

    solving Minesweeper

--- Programming_Challenges/Chapter01/The_3n_plus_one.cpp
+++ Programming_Challenges/Chapter01/The_3n_plus_one.cpp
@@ -4,13 +4,22 @@ typedef long long ll;
 
 ll solve(ll n)
 {
-    if (n == 1)
-        return (1);
-    if (n % 2 == 0)
-        return (1 + solve(n / 2));
-    else
-        return (1 + solve(3 * n + 1));
-    return (0);
+    ll count = 1;
+    ll x = n;
+    while (x != 1)
+    {
+        if (x % 2 == 0)
+        {
+            x = x / 2;
+            count++;
+        }
+        else
+        {
+            x = x * 3 + 1;
+            count++;
+        }
+    }
+    return (count);
 }
 
 void ft_swap(ll *a, ll *b)
@@ -27,17 +36,26 @@ int main(void)
     ll a,b;
     ll res;
     ll max = -1;
+    int  check =0;
     while (scanf("%lld %lld",&a, &b) != EOF)
     {
+        check = 0;
         if (b < a)
+        {
+            check = 0;
             ft_swap(&a,&b);
+        }
         max = -1;
-        for (ll i=a;i<=b;i++)
+        ll i = a;
+        for (;i<=b;i++)
         {
             res = solve(i);
             if (res > max)
                 max = res;
         }
-        printf("%lld %lld %lld\n", a,b,max);
+        if (check)
+            printf("%lld %lld %lld\n", b,a,max);
+        else
+            printf("%lld %lld %lld\n", a,b,max);
     }
 }
