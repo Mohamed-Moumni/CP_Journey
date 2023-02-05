9008a08e95c46927d012567255cf37dbf58e853c
Mohamed Moumni <mmoumni@student.1337.ma>
2023-02-05 13:35:22 +0100

    solving 22_generate_parantheses in cpp

---
+++ LeetCode/Medium/CPP/22_Generate_parantheses.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<string>
+
+using namespace std;
+
+class Solution {
+    vector<string> res;
+public:
+    void    func(string s, int l, int r)
+    {
+        int x;
+        string se;
+        if (l == 0 && r == 0)
+        {
+            res.push_back(s);
+            return ;
+        }
+        if (l == r)
+        {
+            x = l;
+            se = s + "(";
+            func(se, --x, r);
+        }
+        else
+        {
+            if (l != 0)
+            {   
+                x = l;
+                se = s + "(";
+                func(se, --x, r);
+            }
+            if (r != 0)
+            {
+                x = r;
+                se = s + ")";
+                func(se, l, --x);
+            }
+        }
+    }
+    vector<string> generateParenthesis(int n)
+    {
+        string uu = "(";
+        func(uu, n-1, n);
+        return (res);
+    }
+};
+
+int main(void)
+{
+    Solution sol;
+
+    vector<string> result = sol.generateParenthesis(3);
+
+    for (int i = 0;i < result.size();i++)
+    {
+        cout << result[i] << endl;
+    }
+    return (0);
+}
