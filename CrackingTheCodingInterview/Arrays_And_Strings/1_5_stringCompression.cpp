948abc0cc721e0ba74d289eb76ba5be2ccd9f089
Mohamed-Moumni <mohamedmoumni593@gmail.com>
2023-03-02 22:08:51 +0100

    my second solution after review the other solution

--- CrackingTheCodingInterview/Arrays_And_Strings/1_5_stringCompression.cpp
+++ CrackingTheCodingInterview/Arrays_And_Strings/1_5_stringCompression.cpp
@@ -5,48 +5,89 @@
 
 using namespace std;
 
-string  solution(string input)
-{
-    vector <pair<char, int> >   vect;
-    int                         count = 0;
-    int                         val;
-    char                        a = input[0];
-    int                         i = 0;
+// My first attempt
 
-    while (i < input.size())
-    {
-        count = 0;
-        while (input[i] && a == input[i])
-        {
-            count++;
-            i++;
-        }
-        vect.push_back(make_pair(a,count));
-        a = input[i];
-    }
+// string  solution(string input)
+// {
+//     vector <pair<char, int> >   vect;
+//     int                         count = 0;
+//     int                         val;
+//     char                        a = input[0];
+//     int                         i = 0;
+
+//     while (i < input.size())
+//     {
+//         count = 0;
+//         while (input[i] && a == input[i])
+//         {
+//             count++;
+//             i++;
+//         }
+//         vect.push_back(make_pair(a,count));
+//         a = input[i];
+//     }
+//     count = 0;
+//     for (int i = 0; i < vect.size(); i++)
+//     {
+//         val = vect[i].second;
+//         count++;
+//         while (val > 0)
+//         {
+//             count++;
+//             val /= 10;
+//         }
+//     }
+//     if (count >= input.size())
+//         return (input);
+//     string result = "";
+//     for (int i = 0; i < vect.size(); i++)
+//     {
+//         result += vect[i].first;
+//         result += to_string(vect[i].second);
+//     }
+//     return (result);
+// }
+
+int count_compress(string input)
+{
+    int count;
+    int consecutive = 0;
+
     count = 0;
-    for (int i = 0; i < vect.size(); i++)
+    for (int i = 0; i < input.size(); i++)
     {
-        val = vect[i].second;
-        count++;
-        while (val > 0)
+        if ((i + 1 >= input.size()) && (input[i] != input[i+1]))
         {
-            count++;
-            val /= 10;
+            count += to_string(consecutive).size() + 1;
+            consecutive = 0;
         }
+        consecutive++;
     }
-    if (count >= input.size())
-        return (input);
-    string result = "";
-    for (int i = 0; i < vect.size(); i++)
+    return (count);
+}
+
+string compress(string input)
+{
+    if (input.size() <= count_compress(input))
+        return input;
+    string  compressed = "";
+    int     count;
+
+    count = 1;
+    for (int i = 0; i < input.size(); i++)
     {
-        result += vect[i].first;
-        result += to_string(vect[i].second);
+        if (i + 1 >= input.size() || (input[i] != input[i+1]))
+        {
+            compressed += input[i];
+            compressed.append(to_string(count));
+            count = 0;
+        }
+        count++;
     }
-    return (result);
+    return compressed;
 }
 
 int main(void)
 {
-    cout << solution("aabcccccaaa") << endl;
+    cout << compress("aabcccccaaa") << endl;
 }
