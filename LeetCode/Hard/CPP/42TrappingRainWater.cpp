38a5b5ca85468672da8779f01d2fbab67e7b811d
Mohamed-Moumni <mmoumni@student.1337.ma>
2023-05-02 12:29:23 +0100

    solution with O(n) in memory

--- LeetCode/Hard/CPP/42TrappingRainWater.cpp
+++ LeetCode/Hard/CPP/42TrappingRainWater.cpp
@@ -3,50 +3,81 @@
 
 using namespace std;
 
-class Solution {
-public:
-    int trap(vector<int>& height)
-    {
-        int ans = 0;
-        int comp = height[0];
-        int count = 0;
-        int i = 1, j = 0;
-        while (i < height.size())
+// class Solution {
+// public:
+//     int trap(vector<int>& height)
+//     {
+//         int ans = 0;
+//         int comp = height[0];
+//         int count = 0;
+//         int i = 1, j = 0;
+//         while (i < height.size())
+//         {
+//             if (height[i] >= comp)
+//             {
+//                 ans += (comp * (i - j - 1) - count);
+//                 comp = height[i];
+//                 j = i;
+//                 count = 0;
+//             }
+//             else
+//             {
+//                 count += height[i];
+//             }
+//             i++;
+//         }
+//         comp = height[height.size() - 1];
+//         j = height.size() - 1;
+//         i = j - 1;
+//         count = 0;
+//         while (i >= 0)
+//         {
+//             if (height[i] > comp)
+//             {
+//                 ans += (comp * (j - i - 1) - count);
+//                 comp = height[i];
+//                 count = 0;
+//                 j = i;
+//             }
+//             else
+//             {
+//                 count += height[i];
+//             }
+//             i--;
+//         }
+//         return ans;
+//     }
+// };
+
+class Solution
+{
+    public:
+        int trap(vector<int>&height)
         {
-            if (height[i] >= comp)
+            int size = height.size();
+            vector<int> maxleft(size, 0);
+            vector<int> maxright(size, 0);
+
+            int i = 0;
+            int left = height[0];
+            int right = height[size - 1];
+            while (i < size)
             {
-                ans += (comp * (i - j - 1) - count);
-                comp = height[i];
-                j = i;
-                count = 0;
+                if (height[i] > left)
+                    left = height[i];
+                if (height[size-i-1] > right)
+                    right = height[size-i-1];
+                maxleft[i] = left;
+                maxright[size-i-1] = right;
+                i++;
             }
-            else
+            int ans = 0;
+            for (int i = 0; i < size; i++)
             {
-                count += height[i];
+                ans += abs(min(maxleft[i], maxright[i]) - height[i]);
             }
-            i++;
+            return (ans);
         }
-        comp = height[height.size() - 1];
-        j = height.size() - 1;
-        i = j - 1;
-        count = 0;
-        while (i >= 0)
-        {
-            if (height[i] > comp)
-            {
-                ans += (comp * (j - i - 1) - count);
-                comp = height[i];
-                count = 0;
-                j = i;
-            }
-            else
-            {
-                count += height[i];
-            }
-            i--;
-        }
-        return ans;
-    }
 };
 
 int main(void)
