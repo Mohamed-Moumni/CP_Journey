bc6422bf3dd0fa30cd8125666bfd1e4b5dceccd8
Mohamed-Moumni <mmoumni@student.1337.ma>
2023-05-11 20:46:44 +0100

    solve 10001stprime another approach

--- ProjectEuler/10001stPrime.cpp
+++ ProjectEuler/10001stPrime.cpp
@@ -3,31 +3,59 @@
 
 using namespace std;
 
+// this solution is valid but found that it's not all the cases it's work
+
+// int main(void)
+// {
+//     vector<int> primes;
+
+//     primes.push_back(2);
+//     int count = 1;
+//     int i = 3;
+//     while (true)
+//     {
+//         int j = 0;
+//         while (j < primes.size())
+//         {
+//             if (i % primes[j] == 0)
+//                 break;
+//             j++;
+//         }
+//         if (j == primes.size())
+//         {
+//             primes.push_back(i);
+//             count++;
+//         }
+//         if (count == 10001)
+//             break ;
+//         i++;
+//     }
+//     cout << primes[primes.size() - 1] << endl;
+//     return (0);
+// }
+
+bool    isPrime(int n)
+{
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return (true);
+}
+
 int main(void)
 {
-    vector<int> primes;
-
-    primes.push_back(2);
     int count = 1;
     int i = 3;
     while (true)
     {
-        int j = 0;
-        while (j < primes.size())
-        {
-            if (i % primes[j] == 0)
-                break;
-            j++;
-        }
-        if (j == primes.size())
-        {
-            primes.push_back(i);
+        if (isPrime(i))
             count++;
-        }
         if (count == 10001)
             break ;
         i++;
     }
-    cout << primes[primes.size() - 1] << endl;
+    cout << i << endl;
     return (0);
 }
