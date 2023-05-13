e1e7b33a659d6b7cfdda055c91b5aa9537fc51e5
Mohamed-Moumni <mmoumni@student.1337.ma>
2023-05-13 23:33:39 +0100

    solve Maximum subarry sum

--- CSES/SortingAndSearching/MaximumSubarraySumII.cpp
+++ CSES/SortingAndSearching/MaximumSubarraySumII.cpp
@@ -1,42 +1,27 @@
 #include<iostream>
 #include<vector>
-#include<set>
 
 using namespace std;
 
-int solve(vector<int> & vec, int k)
-{
-    long long  ans;
-    long long sum = 0;
-    for (int i = 0; i < k; i++)
-        sum += vec[i];
-    ans = sum;
-    for (size_t i = 0; i < vec.size() - k; i++)
-    {
-        sum -= vec[i];
-        sum += vec[i + k];
-        if (ans < sum)
-            ans = sum;
-    }
-    return (ans);
-}
-
 int main(void)
 {
-    int n,a,b;
-    long long ans = -INT32_MAX;
-    cin >> n >> a >> b;
-
+    int n;cin>>n;
+    int a,b; cin >> a >> b;
     vector<int> vec(n);
-    for (int i = 0; i < n; i++)
+    vector<int> prefixsum(n + 1, 0);
+    for (int i = 1; i <= n; i++)
     {
-        cin >> vec[i];   
+        cin >> vec[i];
+        prefixsum[i] = prefixsum[i - 1] + vec[i];
     }
-    for (int i = a; i <= b;i++)
+    int i = 1, j = b;
+    int ans = -INT32_MAX;
+    while (j <= n - b)
     {
-        long long c = solve(vec,i);
-        if (ans < c)
-            ans = c;
+        for (int k = i; k < i + b;k++)
+            ans = max(ans, prefixsum[k] - prefixsum[i-1]);
+        i++;
+        j++;
     }
     cout << ans << endl;
     return (0);
