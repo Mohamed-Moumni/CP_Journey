2645077e3ad9a20b6fa621a897576510e76470b3
Mohamed-Moumni <mmoumni@student.1337.ma>
2023-08-27 14:03:24 +0100

    solve 21MergeTwoSortedLists

---
+++ LeetCode/Easy/CPP/21MergeTwoSortedLists.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode*   mergeTwoLists(ListNode* list1, ListNode* list2)
+    {
+        ListNode*   l1 = list1;
+        ListNode*   l2 = list2;
+        ListNode*   tail;
+        ListNode    head;
+
+        head.next = nullptr;
+        tail = &head;
+        while (true)
+        {
+            if (l1 == nullptr)
+            {
+                tail->next = l2;
+                break;
+            }
+            else if (l2 == nullptr)
+            {
+                tail->next = l1;
+                break;
+            }
+            if (l1->val < l2->val)
+            {
+                tail->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        return head.next;
+    }      
+};
