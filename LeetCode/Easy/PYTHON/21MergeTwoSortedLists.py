a4c99a8b51fbde8ad0bf8e4e2f95286011097e22
Mohamed-Moumni <mmoumni@student.1337.ma>
2023-08-27 14:25:36 +0100

    add 21MergeTwoSortedLists in python

---
+++ LeetCode/Easy/PYTHON/21MergeTwoSortedLists.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode()
+        tail = head
+
+        while True:
+            if list1 == None:
+                tail.next = list2
+                break
+            elif list2 == None:
+                tail.next = list1
+                break
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        return head.next
