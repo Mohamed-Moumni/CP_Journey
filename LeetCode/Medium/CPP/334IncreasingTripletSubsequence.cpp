4c3da38e9a7fc403d2ab5db21f12e8cd8723b1aa
Mohamed-Moumni <mmoumni@student.1337.ma>
2023-09-01 11:02:17 +0100

    add: 334 Increasing Triplet subsequences problem

--- LeetCode/Medium/CPP/334IncreasingTripletSubsequence.cpp
+++ LeetCode/Medium/CPP/334IncreasingTripletSubsequence.cpp
@@ -1,37 +1,39 @@
+#include <climits>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-bool increasingTriplet(vector<int>& nums)
-{
-    int size = nums.size();
-    int a = INT32_MAX;
-    int b = INT32_MAX;
-
-    for (int i = 0; i < size; i++)
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums)
     {
-        if (b < nums[i])
-            return true;
-        if (a > nums[i]) a = nums[i];
-
-        if (a < nums[i] && nums[i] < b)
-            b = nums[i];
+        int a = INT_MAX;
+        int b = INT_MAX;
+        for (int u: nums)
+        {
+            if (u <= a)
+                a = u;
+            else if (u <= b)
+                b = u;
+            else
+                return true;
+        }
+        return false;
     }
-    return false;
-}
-
+};
 
 int main(void)
 {
     vector<int> a;
+    Solution s;
     a.push_back(20);
     a.push_back(100);
     a.push_back(10);
     a.push_back(12);
     a.push_back(5);
     a.push_back(13);
-    if (increasingTriplet(a))
+    if (s.increasingTriplet(a))
         cout << "True\n";
     else
         cout << "False\n";
